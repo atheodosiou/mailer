e90341fb4c674cc9c3518b70296bad08b80f0623
tasos <theodosiout@or-co.gr>
2020-04-04 02:07:05 +0300

    Adds and fixes

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ dotenv.config();
 
 app.use(express.json());
 
+//Configure cors
 const options = {
   allowedHeaders: [
     "Origin",
@@ -37,15 +38,20 @@ const options = {
 app.use(cors(options));
 
 //Set SendGrid API key
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 //Use routes
 app.use("/api", cors(),routes);
 
 //Error handling 404
 app.use('/', (req, res, next) => {
-    res.status(404).json({code:404,'message':'Not Found'});
+    res.status(404).json({code:404,'error':'Not Found'});
 })
+
+//General error handlign
+app.use((error,req, res, next) => {
+  res.status(error.status || 500).json({code:error.status || 500,'error':error.message || 'Internal Server Error'});
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}...`);
--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,26 +1,25 @@
 const sgMail = require("@sendgrid/mail");
 
 const sendMail = (req, res, next) => {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const message = req.body;
   
   if (!message.to) {
     return res.status(400).json({ error: "Provide to property!" });
   }
   if (!message.from) {
-    return res.status(400).json({ error: "Provide from property!" });
+    return res.status(400).json({ code:400,error: "Provide from property!" });
   }
   if (!message.subject) {
-    return res.status(400).json({ error: "Provide subject property!" });
+    return res.status(400).json({ code:400,error: "Provide subject property!" });
   }
   if (!message.text && !message.html) {
-    return res.status(400).json({ error: "Provide text or html property!" });
+    return res.status(400).json({ code:400,error: "Provide text or html property!" });
   }
 
   sgMail
     .send(message)
     .then((success) => {
-      res.status(200).json({ message: "Mail send" });
+      res.status(200).json({ code:200,message: "Mail send" });
     })
     .catch((error) => {
       res.status(error.status || 500).json({
